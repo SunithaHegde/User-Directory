415a992607fcc0cd38e5dc2e72378e2a1186e2cb
sunitha <sunitha.hegde1992@gmail.com>
2019-01-13 19:33:40 +0530

    auto sugestion and scroll

--- userDirectoryVlocity.js
+++ userDirectoryVlocity.js
@@ -1,18 +1,11 @@
-
 class userDirectoryApp {
 		constructor(){
 			this.searchComponent = null;
 			this.userList = null;
+			this.totalCount = 600;
 			this.getUserList().then((value) => {
 				this.userList = value["People"];
-				var documentFragment = document.createDocumentFragment();
-				this.userList.forEach((val) => {
-					var taskComponent = document.createElement('contact-item');
-					taskComponent.setAttribute("data-name",val.name);
-					taskComponent.setContactDetails(val);
-					documentFragment.appendChild(taskComponent);
-				});
-				document.getElementById("contactList").appendChild(documentFragment);
+				this.createUserComponent(this.userList);
 				let searchComponent = document.createElement('search-component');
 				searchComponent.setUserList(value["People"]);
 				this.searchComponent = searchComponent;
@@ -21,15 +14,14 @@ class userDirectoryApp {
 					taskScope:this
 				});
 				document.getElementById("searchBar").appendChild(this.searchComponent);
+				document.getElementById("contactList").addEventListener('scroll', {handleEvent:this.onScrollHandler,taskScope:this});
 			});	
-			
-
 		}
 		getUserList() {
 			return new Promise((resolve,reject) => {
 					const HTTP = new XMLHttpRequest();
 					HTTP.responseType = 'json';
-					HTTP.open("GET","https://api.myjson.com/bins/e5mq0");
+					HTTP.open("GET","https://api.myjson.com/bins/tff5s");
 					HTTP.send();
 					HTTP.onreadystatechange = (e) => {
 						if (HTTP.readyState == 4 && HTTP.status == 200) {
@@ -38,20 +30,40 @@ class userDirectoryApp {
 					}
 			}); 
 		}	
-		searchHandler(e){
-			let inputVal = this.taskScope.searchComponent.getAttribute("data-value");
-			var documentFragment = document.createDocumentFragment();
-			var filteredList = this.taskScope.userList.filter(function(item){
-				return item.name.toLowerCase().includes(inputVal.toLowerCase());
-			});
-			filteredList.forEach((val) => {
-				var taskComponent = document.createElement('contact-item');
+		createUserComponent(list){
+			let documentFragment = document.createDocumentFragment();
+			list.forEach((val) => {
+				let taskComponent = document.createElement('contact-item');
 				taskComponent.setAttribute("data-name",val.name);
 				taskComponent.setContactDetails(val);
 				documentFragment.appendChild(taskComponent);
 			});
-			document.getElementById("contactList").innerHTML = "";
 			document.getElementById("contactList").appendChild(documentFragment);
+		}	
+		searchHandler(e){
+			if(e.key ==undefined ||e.key.toLowerCase() === "enter"){
+				let inputVal = this.taskScope.searchComponent.getAttribute("data-value");
+				let documentFragment = document.createDocumentFragment();
+				const filteredList = this.taskScope.userList.filter(function(item){
+					return item.name.toLowerCase().includes(inputVal.toLowerCase());
+				});
+				filteredList.forEach((val) => {
+					let taskComponent = document.createElement('contact-item');
+					taskComponent.setAttribute("data-name",val.name);
+					taskComponent.setContactDetails(val);
+					documentFragment.appendChild(taskComponent);
+				});
+				document.getElementById("contactList").innerHTML = "";
+				document.getElementById("contactList").appendChild(documentFragment);
+			}
+		}	
+		onScrollHandler(e){
+			if(document.getElementById("contactList").scrollTop + document.getElementById("contactList").clientHeight >= document.getElementById("contactList").scrollHeight){
+				this.taskScope.userList.length < this.taskScope.totalCount && this.taskScope.getUserList().then((value) => {
+					this.taskScope.userList = this.taskScope.userList.concat(value["People"]);
+					this.taskScope.createUserComponent(value["People"]);
+				});	
+			}
 		}	
 }	
 
@@ -145,8 +157,8 @@ customElements.define('contact-details',
 			let numberOfRows = user.Likes > user.Dislikes ? user.Likes.length : user.Dislikes.length;
 			for (let i = 0; i < numberOfRows; i++) {
 				let row = document.createElement("tr");
-				for (var j = 0; j < 2; j++) {
-				  var cell = document.createElement("td");
+				for (let j = 0; j < 2; j++) {
+				  let cell = document.createElement("td");
 				  let textContent = j == 0? user.Likes[i]: user.Dislikes[i];
 				  textContent = textContent == undefined?"":textContent;
 				  let cellText = document.createTextNode(textContent);
@@ -173,7 +185,6 @@ customElements.define('search-component',
 				margin: 50px 0 0 13px;
 				padding: 5px 37px 2px 38px;
 				width:100%;
-				font-family: FontAwesome;
 				font-style: normal;
 				font-weight: normal;
 				text-decoration: inherit;
@@ -182,10 +193,30 @@ customElements.define('search-component',
 				background-repeat: no-repeat;
 			}
 			.awesomeSearch_search_children {
-			  height:20px;
-			  margin: 0 0 0 15px;
-			  width:100%;
+			  height:35px;
+			  background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAACY0lEQVQ4jaWUMWtTURzFf+dRggQpIiV0KKUvYt8rFOkHcOgkItipIg4quih+AXEopUMXBZcujiqCH6FLqYuLdbGl+G5A8qRTKUEkSAhFchxeAjFJreIZL//745zzv1wZM6g8CxWkaeAcEGE3gQOkozhNfg5d6JP6gXkWykhL2HeRUuxJpMjQAILsTaQXcZr8OBWYh9oUsGZYFozbBjhGQlCyDVJbsGX7SXUu3T8RmGehbNiQdB/AcCh7G2kX6AAXgSvYM0gA28CtOE2OBoFjBVZLspe7rurAA6SdOE2afVUsIK0Di8Ci4X4eak/jNOn85rAeQgX7leEq0iFwu5omW6Pi5FmYQXoHzGAHpOtxmnzpn4mAaaQUCdnbgp2TCkc6AN5QJKlgLwyORMB5YBL7GGm3F3OUuvE+WGohjSNNDQG7vUUqyu4MDoxQR8Vc5N4O+oGSfhi+ASXgQj0L5ZNIeahFwCx2GWgJhrYcAQfAZwO2rwCX/uBuArjnItmRYW8U8FCwKWhLqkpaz7MwlWch6neWh1oFew2Y79bzHvg8CCwedqiNA2+BawDYX5FeAx+xO0izwD1g3hCp6H3F0vNqmrSGgAB5Fi4hbRguq/gQQGoBHeyye4srzgEa2CtIL+M0afdH7qK1D9wAVg0B+Ga7hF1Gagnqtl8DK9gN2xNIz4CHeaidGXLYUz3UIkEVWACmDGOyG5Y+dTsbk30H6Znts5K+A0+Al3GatIeAf6M81ErAI+xVpHO2A3CzOpfuRaddHqU4TY6xXxgeA0FSAJojI/+L6lk4I5g1NCUdxGnS+S/gKP0CIS8i4nYwM7QAAAAASUVORK5CYII=);
+			  background-position: 5px 15px;
+			  background-repeat: no-repeat;
+			  text-overflow:ellipsis;
+			  overflow:hidden;
+			  white-space:nowrap;
+			  padding-left:30px;
+			  padding-top: 15px;
+			  cursor:pointer;
 			}
+			.awesomeSearch_search_children:hover{
+				background-color:#e0e4ea;
+			}	
+			#autoSearchContainer{
+				position: relative;
+				z-index: 1;
+				background: white;
+				width: 100%;
+				margin: 0px 0 0 13px;
+				box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
+				max-height: 200%;
+				overflow-y:auto;
+			}	
 			</style>
 			<input class="search" id="search" type="search" placeholder="Search">`;
 			let shadow = this.attachShadow({mode: 'open'});
@@ -195,13 +226,50 @@ customElements.define('search-component',
 				handleEvent:this.searchHandler,
 				taskScope:this
 			});
+			this.searchComponent.addEventListener("blur",(e) =>{
+				let autoContainer = this.shadowRoot.querySelector("#autoSearchContainer");
+				if( autoContainer && autoContainer.querySelectorAll( ":hover" ).length == 0){
+					autoContainer.style.display = "none";
+				}	
+			});
 		}
 		setUserList(list){
 			this.userList = list;	
 		}	
 		searchHandler(e){
-			this.taskScope.setAttribute("data-value",this.taskScope.searchComponent.value);
-		}	
+			let inputValue = this.taskScope.searchComponent.value.toLowerCase();
+			let autoContainer = this.taskScope.shadowRoot.querySelector("#autoSearchContainer");
+			if(e.key == undefined || e.key.toLowerCase() ==="enter"){
+				this.taskScope.setAttribute("data-value",inputValue);
+			} else {
+				if(inputValue == ""){
+					autoContainer && this.taskScope.shadowRoot.removeChild(autoContainer);
+				} else {
+					let list = this.taskScope.userList.filter((item)=>{
+						return item.name.toLowerCase().includes(inputValue);
+					});
+					let searchChildren ="";
+					let newElement = document.createElement("div");
+					newElement.id= "autoSearchContainer";
+					for(let i =0; i< list.length ;++i){
+						searchChildren += `<div class='awesomeSearch_search_children'>${list[i].name}</div>`
+					}
+					newElement.innerHTML = searchChildren;
+					autoContainer && this.taskScope.shadowRoot.removeChild(autoContainer);
+					newElement.addEventListener("click",(e) =>{
+						if (e.target !== e.currentTarget) {
+							this.taskScope.setAttribute("data-value",e.target.textContent);
+							this.taskScope.searchComponent.value = e.target.textContent;
+							newElement.remove();
+							this.taskScope.searchComponent.focus();
+							this.taskScope.searchComponent.dispatchEvent(new CustomEvent("keyup"));
+						}
+						e.stopPropagation();
+					});
+					this.taskScope.shadowRoot.appendChild(newElement);
+				}	
+			}	
+		}		
 	}
 );	
 new userDirectoryApp();
